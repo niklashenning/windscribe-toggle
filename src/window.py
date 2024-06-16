b78e5a2ae90328ba58efc42cbcfe9ab2b61c90b3
Niklas Henning <niklashenningbusiness@gmail.com>
2024-06-16 11:34:00 +0200

    Update example to use stateChanged

--- src/window.py
+++ src/window.py
@@ -15,7 +15,6 @@ class Window(QMainWindow):
 
         # Create toggle button
         self.toggle_button = ToggleButton(self)
-        self.toggle_button.clicked.connect(self.toggle_button_clicked)
         self.toggle_button.stateChanged.connect(self.toggle_button_state_changed)
 
         # Create timer for toggle button
@@ -29,8 +28,8 @@ class Window(QMainWindow):
         central_widget.setLayout(main_layout)
         self.setCentralWidget(central_widget)
 
-    def toggle_button_clicked(self):
-        if self.toggle_button.getState() != ToggleButtonState.OFF:
+    def toggle_button_state_changed(self, state):
+        if state == ToggleButtonState.TURNING_ON:
             # Start timer that sets the state of the toggle button to ON after a
             # random delay of 1750 - 3000 milliseconds if the state is still TURNING_ON
             # (You would normally run your turning on functionality here instead of a random delay)
@@ -39,14 +38,10 @@ class Window(QMainWindow):
             self.toggle_button_timer.timeout.connect(self.toggle_button_timer_finished)
             self.toggle_button_timer.start(random.randint(1750, 3000))
         else:
-            # If the button has been clicked and turned off again, stop timer
+            # If the button is not in TURNING_ON state, stop timer
             self.toggle_button_timer.stop()
-
-    def toggle_button_state_changed(self, state):
-        # Print the new state
-        print(state)
 
     def toggle_button_timer_finished(self):
         # If the state is still TURNING_ON, set the state to ON
-        if self.toggle_button.state == ToggleButtonState.TURNING_ON:
+        if self.toggle_button.getState() == ToggleButtonState.TURNING_ON:
             self.toggle_button.setState(ToggleButtonState.ON)
